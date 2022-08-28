1a187c6b1e39c38e2f377f7a29863d8399600d9c
Ritik Chourasiya <ritvikchoure65@gmail.com>
2022-08-28 17:14:54 +0530

    commit

--- DS/tree.cpp
+++ DS/tree.cpp
@@ -60,6 +60,120 @@ class BST {
             }
         }
     }
+
+    void printPreorder(TreeNode *r) {
+        if(r == NULL) {
+            return;
+        } else {
+            cout << r->value << " ";
+            printPreorder(r->left);
+            printPreorder(r->right);
+        }
+    }
+
+    void printInorder(TreeNode *r) {
+        if(r == NULL) {
+            return;
+        } else {
+            printInorder(r->left);
+            cout << r->value << " ";
+            printInorder(r->right);
+        }
+    }
+    
+    void printPostorder(TreeNode *r) {
+        if(r == NULL) {
+            return;
+        } else {
+            printPostorder(r->left);
+            printPostorder(r->right);
+            cout << r->value << " ";
+        }
+    }
+
+    void searchValue(int val) {
+        if(root == NULL) {
+            return;
+        } else {
+            TreeNode *temp = root;
+            while(temp != NULL) {
+                if(val == temp->value) {
+                    cout << "Found" << endl;
+                    return;
+                } else if(val > temp->value) {
+                    temp = temp->right;
+                } else {
+                    temp = temp->left;
+                }
+            }
+        }
+    }
+
+    int height(TreeNode *node) {
+        if(node == NULL) {
+            return -1;
+        } else {
+            int leftHeight = height(node->left);
+            int rightHeight = height(node->right);
+
+            if(leftHeight > rightHeight) {
+                return leftHeight + 1;
+            } else {
+                return rightHeight + 1;
+            }
+        }
+    }
+
+    void printBFSOrder(TreeNode *r) {
+        int h = height(r);
+        for(int i = 0; i <= h; i++) {
+            printGivenLevel(r, h);
+        }
+    }
+
+    void printGivenLevel(TreeNode *r, int level) {
+        if(r == NULL) {
+            return;
+        } else if(level == 0) {
+            cout << r->value << " ";
+        } else {
+            printGivenLevel(r->left, level-1);
+            printGivenLevel(r->right, level-1);
+        }
+    }
+
+    TreeNode* minValueNode(TreeNode* r) {
+        TreeNode* current = root;
+        while(current != NULL) {
+            current = current->left;
+        }
+
+        return current;
+    }
+
+    TreeNode* deleteNode(TreeNode* r, int val) {
+        if(r == NULL) {
+            return;
+        } else if(val > r->value) {
+            r->right = deleteNode(r->right, val);
+        } else if(val < r->value) {
+            r->left = deleteNode(r->left, val);
+        } else {
+            if(r->left == NULL) {
+                TreeNode* temp = r->right;
+                delete r;
+                return temp;
+            } else if(r->right == NULL) {
+                TreeNode* temp = r->left;
+                delete r;
+                return temp;
+            } else {
+                TreeNode* temp = minValueNode(r->right);
+                r->value = temp->value;
+                r->right = deleteNode(r->right, temp->value);
+            }
+        }
+    }
 };
 
 
@@ -75,8 +189,12 @@ int main() {
         cout << "1. Insert Node" << endl;
         cout << "2. Search Node" << endl;
         cout << "3. Delete Node" << endl;
-        cout << "4. Print BST Values" << endl;
-        cout << "5. Clear screen" << endl;
+        cout << "4. Print Pre Order Values" << endl;
+        cout << "5. Print In order Values" << endl;
+        cout << "6. Print Post order Values" << endl;
+        cout << "7. Print BFS Order of Tree" << endl;
+        cout << "8. Print Height of Tree" << endl;
+        cout << "9. Clear screen" << endl;
         cout << "0. Exit" << endl;
 
         cin >> option;
@@ -98,6 +216,8 @@ int main() {
 
             case 2:
                 cout << "Search" << endl;
+                cin >> val;
+                tree.searchValue(val);
                 break;
 
             case 3:
@@ -106,9 +226,29 @@ int main() {
 
             case 4:
                 cout << "Print" << endl;
+                tree.printPreorder(tree.root);
+                break;
+            
+            case 5:
+                cout << "Print" << endl;
+                tree.printInorder(tree.root);
+                break;
+            
+            case 6:
+                cout << "Print" << endl;
+                tree.printPostorder(tree.root);
+                break;
+           
+            case 7:
+                cout << "Print" << endl;
+                tree.printBFSOrder(tree.root);
                 break;
 
-            case 5:
+            case 8: 
+                cout << "Height " << tree.height(tree.root) << endl;
+                break;
+
+            case 9:
                 system("cls");
                 break;  
 
